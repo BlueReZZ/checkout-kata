9341cb24ff156ecd1e17565995c463e285142947
Paul Shannon <paul@paulshannon.com>
2013-02-11 17:39:36 +0000

    tidied up

--- CheckoutKata/CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutKata/CheckoutTests.cs
@@ -34,28 +34,34 @@ namespace CheckoutKata
 
     public class Checkout
     {
+        private readonly Dictionary<char, int> _prices;
+        private readonly Dictionary<char, int> _counts;
+
+        public Checkout()
+        {
+            _prices = new Dictionary<char, int>{{'A', 50}, {'B', 30}};
+            _counts = new Dictionary<char, int>();
+        }
+
         public int PriceFor(string skus)
         {
             var total = 0;
-            var countOfA = 0;
-            var prices = new Dictionary<char, int>{{'A', 50}, {'B', 30}};
-            var counts = new Dictionary<char, int>();
 
             if (string.IsNullOrEmpty(skus))
                 return total;
             
             foreach (var sku in skus)
             {
-                if (counts.ContainsKey(sku))
-                    counts[sku]++;
+                if (_counts.ContainsKey(sku))
+                    _counts[sku]++;
                 else
-                    counts[sku] = 1;
+                    _counts[sku] = 1;
 
-                total += prices[sku];
+                total += _prices[sku];
             }
 
-            if(counts.ContainsKey('A'))
-                total -= 20 * (counts['A'] / 3);
+            if(_counts.ContainsKey('A'))
+                total -= 20 * (_counts['A'] / 3);
 
             return total;
         }
