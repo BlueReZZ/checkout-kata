9b46f18efd3f462fe63d488efa8d0a06ad03c914
Paul Shannon <paul@paulshannon.com>
2013-02-11 17:12:48 +0000

    inital test

---
+++ CheckoutKata/CheckoutKata/CheckoutTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKata
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void EmptyCheckoutHasZeroPrice()
+        {
+            Assert.That(new Checkout().PriceFor(""), Is.EqualTo(0));
+        }
+    }
+
+    public class Checkout
+    {
+        public int PriceFor(string skus)
+        {
+            return 0;
+        }
+    }
+}
