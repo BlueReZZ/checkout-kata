b69a62833f36de3844a772cd2b91941bf4f24495
Paul Shannon <paul@paulshannon.com>
2013-02-11 17:15:58 +0000

    added first test for A

--- CheckoutKata/CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutKata/CheckoutTests.cs
@@ -10,13 +10,23 @@ namespace CheckoutKata
         {
             Assert.That(new Checkout().PriceFor(""), Is.EqualTo(0));
         }
+
+        [Test]
+        [TestCase("A", 50)]
+        public void PriceForListOfSkusIsAsExpected(string skus, int expected)
+        {
+            Assert.That(new Checkout().PriceFor(skus), Is.EqualTo(expected));
+        }
     }
 
     public class Checkout
     {
         public int PriceFor(string skus)
         {
-            return 0;
+            if (string.IsNullOrEmpty(skus))
+                return 0;
+
+            return 50;
         }
     }
 }
