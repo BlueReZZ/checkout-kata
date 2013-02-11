cd53d0c64ec66fd1ddb586f6d90486168b31add4
Paul Shannon <paul@paulshannon.com>
2013-02-11 17:17:10 +0000

    removed duplication of checkout construction

--- CheckoutKata/CheckoutKata/CheckoutTests.cs
+++ CheckoutKata/CheckoutKata/CheckoutTests.cs
@@ -5,17 +5,25 @@ namespace CheckoutKata
     [TestFixture]
     public class CheckoutTests
     {
+        private Checkout _checkout;
+
+        [SetUp]
+        public void Setup()
+        {
+            _checkout = new Checkout();
+        }
+
         [Test]
         public void EmptyCheckoutHasZeroPrice()
         {
-            Assert.That(new Checkout().PriceFor(""), Is.EqualTo(0));
+            Assert.That(_checkout.PriceFor(""), Is.EqualTo(0));
         }
 
         [Test]
         [TestCase("A", 50)]
         public void PriceForListOfSkusIsAsExpected(string skus, int expected)
         {
-            Assert.That(new Checkout().PriceFor(skus), Is.EqualTo(expected));
+            Assert.That(_checkout.PriceFor(skus), Is.EqualTo(expected));
         }
     }
 
